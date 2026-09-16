e3288ee5b1f2819e92face77c5a0ca3ca0bd268b
Leonardo <leonardo_vieira199@hotmail.com>
2026-09-16 08:08:38 -0300

    genres

--- src/main/java/com/devsuperior/movieflix/controllers/GenreController.java
+++ src/main/java/com/devsuperior/movieflix/controllers/GenreController.java
@@ -1,9 +1,29 @@
 package com.devsuperior.movieflix.controllers;
 
+import com.devsuperior.movieflix.dto.GenreDTO;
+import com.devsuperior.movieflix.services.GenreService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/genres")
 public class GenreController {
+
+    private final GenreService genreService;
+
+    public GenreController(GenreService genreService) {
+        this.genreService = genreService;
+    }
+
+
+    @PreAuthorize("hasAnyRole('VISITOR', 'MEMBER')")
+    @GetMapping
+    public ResponseEntity<List<GenreDTO>> findAll() {
+        return ResponseEntity.ok().body(genreService.findAll());
+    }
 }
---
+++ src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.movieflix.services;
+
+import com.devsuperior.movieflix.dto.GenreDTO;
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.repositories.GenreRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GenreService {
+
+    private final GenreRepository genreRepository;
+
+    public GenreService(GenreRepository genreRepository) {
+        this.genreRepository = genreRepository;
+    }
+
+    public List<GenreDTO> findAll() {
+        List<Genre> result = genreRepository.findAll();
+        return result.stream().map(GenreDTO::new).toList();
+    }
+}
